513e8fb72b0483440a995c84d2a14286a62b0419
Olivier Bouchoms <391028@student.fontys>
2021-03-11 14:32:31 +0100

    Add some unsafe code

---
+++ Example/Example.Tests/CommandExecutorTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Example.Tests
+{
+    public class CommandExecutorTests
+    {
+        private readonly CommandExecutor _executor;
+
+        public CommandExecutorTests()
+        {
+            _executor = new CommandExecutor();
+        }
+        
+        [Fact]
+        public void TestCommandExecutor()
+        {
+            _executor.Start(".");
+        }
+    }
+}
---
+++ Example/Example/CommandExecutor.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Xml;
+
+namespace Example
+{
+    public class CommandExecutor
+    {
+        /// <summary>
+        /// Unsafe demonstration method
+        /// </summary>
+        /// <param name="path"></param>
+        public void Start(string path)
+        {
+            var doc = new XmlDocument {XmlResolver = null};
+            doc.Load("/config.xml");
+            var results = doc.SelectNodes("/Config/Devices/Device[id='" + path + "']");
+            
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            
+            var process = new Process();
+            
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "START " + path;
+            
+            process.Start();
+        }
+    }
+}
